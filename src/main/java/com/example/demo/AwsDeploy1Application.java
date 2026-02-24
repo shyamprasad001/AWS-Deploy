f418291b222b4fea635f9ebc44c4e3c8f26852e1
Shyam Mantri <iamshyam1911@gmail.com>
2026-02-24 11:16:15 +0530

    first commit

---
+++ src/main/java/com/example/demo/AwsDeploy1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsDeploy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsDeploy1Application.class, args);
+	}
+
+}
